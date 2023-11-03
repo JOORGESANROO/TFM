6f24b8ce499e7ce4ce5ed657c2ef0cb9a83eee82
JOORGESANROO <145056351+JOORGESANROO@users.noreply.github.com>
2023-11-03 16:12:51 +0100

    Update __init__.py

--- module_structure/__init__.py
+++ module_structure/__init__.py
@@ -1,1 +1,14 @@
+from module_structure1.tensor_operator import *
 
+
+
+MAJOR = 1
+MINOR = 0
+PATCH = 0
+COMMIT = 12
+
+# from main import *
+
+__version__ = f'{MAJOR}.{MINOR}.{PATCH}.{COMMIT}'
+__author__  = 'Universidad Francisco de Vitoria'
+__name__    = 'module_structure'
